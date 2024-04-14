c4cc4ee30331cf0c77eb9fd8d58ede1658c7f669
Aigiz Rakhmatullin <you@example.com>
2024-04-14 12:12:01 +0300

    admin dashboard customisation

--- myfirst/dbcore/admin.py
+++ myfirst/dbcore/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from Djangoproject.myfirst.dbcore.models import DBTypes, Element
+from dbcore.models import DBTypes, Element
 
 admin.site.register(DBTypes)
 admin.site.register(Element)
