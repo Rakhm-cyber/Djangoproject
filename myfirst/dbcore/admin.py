ba3b7dd057220a688fca67cbffc0cbbe551b7d3a
Aigiz Rakhmatullin <you@example.com>
2024-04-18 11:51:17 +0300

    new architecture

--- myfirst/dbcore/admin.py
+++ myfirst/dbcore/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from dbcore.models import DBTypes, Element
+from dbcore.models import Type, Database, Port, Server_type, Server, Database_server
 
-admin.site.register(DBTypes)
-admin.site.register(Element)
+admin.site.register(Type)
+admin.site.register(Database)
+admin.site.register(Port)
+admin.site.register(Server_type)
+admin.site.register(Server)
+admin.site.register(Database_server)
